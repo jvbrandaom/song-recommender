969d0b7d5ab40b7d743711636c30d2c585237a30
João Vitor Brandão Moreira <jvbrandaom@gmail.com>
2018-12-04 00:22:02 -0200

    creates rest endpoint

---
+++ rules-engine/src/main/java/com/gmail/jvbrandaom/rulesengine/controller/RulesController.java
@@ -0,0 +1,21 @@
+package com.gmail.jvbrandaom.rulesengine.controller;
+
+import com.gmail.jvbrandaom.rulesengine.service.RulesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class RulesController {
+
+    @Autowired
+    private RulesService rulesService;
+
+    @GetMapping("/song/genre/{temperature}")
+    public String getGenreBasedOnTemperature(@PathVariable Integer temperature) throws IOException {
+        return rulesService.getSongGenre(temperature);
+    }
+}
